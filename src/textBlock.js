cab83f27a110c88e8acb3d488c46548fcc6cb8a4
Psrijith <peddireddysrijith@gmail.com>
2024-07-07 21:41:21 +0530

    commit

---
+++ src/textBlock.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+function TextBlock() {
+    return (
+        <div id="textblock">
+            <div id="textblock-container">
+                <h1 id="textblock-title">Who is P.Srijith?</h1>
+                <p id="textblock-content">
+                Tech Enthusiast, Web Wizard<br/><br/>
+                P. Srijith, currently pursuing B.Tech in Computer Science at VNRVJIET,
+                 stands poised to leverage his diverse skill set and educational background for
+                  impactful contributions in the tech industry. With a GPA of 8.99 and a strong 
+                  foundation in programming languages including C, C++, Python, and Java, he has
+                   also mastered a spectrum of web technologies such as React JS, NodeJS, and Django.
+                    Srijith's proficiency extends to software tools like AutoCAD, R Studio, and AWS, 
+                    complemented by certifications in CyberSecurity Fundamentals from IBM and Cloud Computing from IIT Kharagpur. 
+                    His academic journey is punctuated by notable projects, including a Movie Recommendation System
+                     using machine learning, and a Pay 1 Know 1 platform integrating Stripe and Firebase. 
+                     His proactive approach is further evidenced through internships at Coding Ninjas, 
+                     where he excelled as a Campus Ambassador, and as a Research Intern at Centella Scientific, 
+                     enhancing research methodologies. Srijith's achievements underscore his prowess in competitive
+                     programming with a Codechef rating of 1634 and stellar performances across platforms like Leetcode 
+                     and HackerRank. Beyond academics, he enjoys chess, creating YouTube content, PC gaming, and exploring
+                      new hacking tools. Selected for prestigious programs like PWC Launchpad'24 and Amazon ML Summer School,
+                       Srijith exemplifies dedication to continual learning and excellence in technology.</p>
+            </div>
+            <br/>
+            <footer id='textblock-footer'>Created With 🧡 By&nbsp;<a id="textblock-Srijith" href="https://www.linkedin.com/in/peddireddy-srijith-ba319325b/">Srijith</a></footer>
+        </div>
+    );
+}
+
+export default TextBlock;
